fde21d9c2ab02509eab3e6f449429955112efeb5
Adrian Gonzales <adriangonzalesval@gmail.com>
2025-11-05 17:53:05 -0500

    fix: Removed unnecessary access

--- src/main/java/io/github/_4drian3d/jdwebhooks/component/AbstractComponentBuilder.java
+++ src/main/java/io/github/_4drian3d/jdwebhooks/component/AbstractComponentBuilder.java
@@ -1,6 +1,6 @@
 package io.github._4drian3d.jdwebhooks.component;
 
-public sealed abstract class AbstractComponentBuilder<R extends Component, T extends ComponentBuilder<R, T>> implements ComponentBuilder<R, T> permits ContainerComponentImpl.BuilderImpl, FileComponentImpl.Builder, MediaGalleryComponentImpl.Builder, SectionComponentImpl.Builder, SeparatorComponentImpl.Builder, TextDisplayComponentImpl.Builder, ThumbnailComponentImpl.Builder {
+sealed abstract class AbstractComponentBuilder<R extends Component, T extends ComponentBuilder<R, T>> implements ComponentBuilder<R, T> permits ContainerComponentImpl.BuilderImpl, FileComponentImpl.Builder, MediaGalleryComponentImpl.Builder, SectionComponentImpl.Builder, SeparatorComponentImpl.Builder, TextDisplayComponentImpl.Builder, ThumbnailComponentImpl.Builder {
   protected Integer id;
 
   @Override
